8fa545f8783820b24bac8d1fa271b180ed7bcdaa
Adailson <adailson2@gmail.com>
2019-07-11 17:54:02 -0300

    PageHeader and TodoForm

--- frontend/src/about/about.jsx
+++ frontend/src/about/about.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import PageHeader from '../template/pageHeader'
 
 export default props => (
     <div>
-        <h1>Sobre</h1>
+        <PageHeader name='Sobre' small='Nós'/>
+        <h2>Nossa História</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum sint, veritatis in incidunt dolore quo enim assumenda similique aperiam molestiae fugit, nemo, veniam tenetur non laudantium ad voluptatem. Blanditiis, libero.</p>
+        <h2>Missão e Visão</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Perspiciatis ipsum nisi sequi, repellendus illo, dolores voluptas, non quisquam velit tenetur animi. Itaque culpa maxime in, molestias ullam impedit id accusantium?</p>
+        <h2>Imprensa</h2>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Neque, eos ullam placeat vitae asperiores unde possimus! Ut voluptate pariatur, corporis sed quod saepe aliquam ipsa nisi laudantium harum dolor eligendi!</p>
     </div>
 )
