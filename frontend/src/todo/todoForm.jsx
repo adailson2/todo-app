9626bcd3806368c3fc58ccb41f0533ce697fd710
Adailson <adailson2@gmail.com>
2019-07-16 13:18:51 -0300

    Implement and integrate todoActions and todoReducer

--- frontend/src/todo/todoForm.jsx
+++ frontend/src/todo/todoForm.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+
 import Grid from '../template/grid'
 import IconButton from '../template/iconButton'
+import { changeDescription } from './todoActions'
 
 const TodoForm = props => {
     const keyHandler = (e) => {
@@ -16,7 +19,7 @@ const TodoForm = props => {
             <Grid cols='12 9 10'>
                 <input id='description' className='form-control' 
                     placeholder="Adicione uma tarefa"
-                    onChange={props.handleChange}
+                    onChange={props.changeDescription}
                     onKeyUp={keyHandler}
                     value={props.description}/>
             </Grid>
@@ -37,4 +40,7 @@ const mapStateToProps = state => ({
   description: state.todo.description
 });
 
-export default connect(mapStateToProps)(TodoForm)
+const mapDispatchToProps = dispatch =>
+  bindActionCreators({ changeDescription }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoForm)
